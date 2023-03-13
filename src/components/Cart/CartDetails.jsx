63154afef90453828da6ec5051f9ee339ca565f4
PatiriJulian <patirijulian94@gmail.com>
2023-03-13 16:25:56 -0300

    fixeando deploy 4

--- src/components/Cart/CartDetails.jsx
+++ src/components/Cart/CartDetails.jsx
@@ -1,4 +1,4 @@
-import { useCartContext } from "../../context/cartContex";
+import { useCartContext } from "../../context/CartContex";
 import Button from 'react-bootstrap/Button';
 
 function CartDetails(){
