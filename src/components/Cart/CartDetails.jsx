2d07a332a47e94bd50033ca528bbc64a5abcf93a
PatiriJulian <patirijulian94@gmail.com>
2023-03-14 11:51:22 -0300

    css en cartDetail

--- src/components/Cart/CartDetails.jsx
+++ src/components/Cart/CartDetails.jsx
@@ -1,12 +1,13 @@
 
 import Button from 'react-bootstrap/Button';
 import { useCartContext } from "../../context/CartContext";
+import './CartDetails.css'
 function CartDetails(){
     const {cartList, borrarItem, total}=useCartContext ();
     return (<> 
         <ul>
             {cartList.map (function(item){
-                return <li>{item.name}, Cantidad: {item.cantidad}  <Button onClick={()=> borrarItem(item.id)}>Borrar</Button></li>
+                return <li><img className='imgCartDetails' src={item.pic}/>{item.name}, Cantidad: {item.cantidad}  <Button onClick={()=> borrarItem(item.id)}>Borrar</Button></li>
             })}
         </ul>
         <p>Total:$ {total()}</p>
